3a5ceb10945241d36098a0d6eeee5dcfd8b0dfbc
akhvatov <akhvatov@gvcgroup.com>
2019-01-28 13:44:40 +0100

    заставил тест работать

--- src/main/java/api/SearchApiJson.java
+++ src/main/java/api/SearchApiJson.java
@@ -8,14 +8,30 @@ import java.util.List;
 
 public class SearchApiJson {
 
-    private String json;
+    private static final String RESULT_JSON_PATH = "RestResponse.result";
 
-    public SearchApiJson(String json) {
-        this.json = json;
+    private final JsonPath jsonPath;
+
+    public static SearchApiJson from(String json) {
+        return new SearchApiJson(json);
     }
 
+    private SearchApiJson(String json) {
+        this.jsonPath = JsonPath.from(json);
+    }
+
+    /**
+     * @return get result for one country
+     */
+    public Result getResult() {
+        return jsonPath.getObject(RESULT_JSON_PATH, Result.class);
+    }
+
+    /**
+     * @return get result for multiple countries in one response
+     */
     public List<Result> getResults() {
-        return JsonPath.from(json).getList("result", Result.class);
+        return jsonPath.getList(RESULT_JSON_PATH, Result.class);
     }
 
 /*    public Set<String> getName() {
--- src/test/java/HelloFresh.java
+++ src/test/java/HelloFresh.java
@@ -1,7 +1,10 @@
+import api.SearchApiJson;
 import api.SearchApiRequest;
+import api.schema.Result;
 import base.BaseClass;
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertEquals;
 
 public class HelloFresh extends BaseClass {
 
@@ -9,6 +12,10 @@ public class HelloFresh extends BaseClass {
     public void test() {
         String searchResultsApiJson = SearchApiRequest.makeSearchRequest();
         System.out.println(searchResultsApiJson);
-        System.out.println(searchResultsApiJson);
+        final SearchApiJson searchApiJson = SearchApiJson.from(searchResultsApiJson);
+        final Result result = searchApiJson.getResult();
+        assertEquals(result.getName(), "Germany");
+        assertEquals(result.getAlpha2_code(), "DE");
+        assertEquals(result.getAlpha3_code(), "DEU");
     }
 }
