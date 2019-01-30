688d65c2974c2d602eae9ac353c02d7bc1900f97
Evgenii Bazhanov <evgenii.bazhanov@smava.de>
2019-01-30 16:46:45 +0100

    upd

--- src/test/java/com/tests/WebTest.java
+++ src/test/java/com/tests/WebTest.java
@@ -20,37 +20,25 @@ public class WebTest {
 
     @BeforeMethod
     public void setUp() {
-/*        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
-        driver = new ChromeDriver();
-        wait = new WebDriverWait(driver, 10, 50);*/
-
         Selenide.open("http://automationpractice.com/index.php");
     }
 
     @Test
     public void signInTest() {
-
         String timestamp = String.valueOf(new Date().getTime());
         String email = "hf_challenge_" + timestamp + "@hf" + timestamp.substring(7) + ".com";
         String name = "Firstname";
         String surname = "Lastname";
-
-
         $(".login").click();
-
         $(By.id("email_create")).setValue(email);
-
         $(By.id("SubmitCreate")).click();
         $(By.id("id_gender2")).click();
-
-
         $(By.id("customer_firstname")).setValue(name);
         $(By.id("customer_lastname")).setValue(surname);
         $(By.id("passwd")).setValue("Qwerty");
         $(By.id("days")).selectOptionByValue("1");
         $(By.id("months")).selectOptionByValue("1");
         $(By.id("years")).selectOptionByValue("2000");
-
         $(By.id("company")).sendKeys("Company");
         $(By.id("address1")).sendKeys("Qwerty, 123");
         $(By.id("address2")).sendKeys("zxcvb");
@@ -62,8 +50,6 @@ public class WebTest {
         $(By.id("phone_mobile")).sendKeys("12345123123");
         $(By.id("alias")).sendKeys("hf");
         $(By.id("submitAccount")).click();
-
-
         $("h1").waitUntil(Condition.visible, 1000).shouldHave(Condition.text("MY ACCOUNT"));
         assertEquals($(By.className("account")).getText(), name + " " + surname);
         assertTrue($(By.className("info-account")).getText().contains("Welcome to your account."));
@@ -78,38 +64,35 @@ public class WebTest {
         $(By.id("email")).setValue(existingUserEmail);
         $(By.id("passwd")).setValue(existingUserPassword);
         $(By.id("SubmitLogin")).click();
-
         $("h1").waitUntil(Condition.visible, 1000).shouldHave(Condition.text("MY ACCOUNT"));
-
         $(".account").shouldHave(Condition.text(fullName));
         assertTrue($(By.className("info-account")).getText().contains("Welcome to your account."));
         assertTrue($(By.className("logout")).isDisplayed());
         assertTrue(url().contains("controller=my-account"));
     }
 
-/*    @Test
+    @Test
     public void checkoutTest() {
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("login"))).click();
-        driver.findElement(By.id("email")).sendKeys(existingUserEmail);
-        driver.findElement(By.id("passwd")).sendKeys(existingUserPassword);
-        driver.findElement(By.id("SubmitLogin")).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Women"))).click();
-        driver.findElement(By.xpath("//a[@title='Faded Short Sleeve T-shirts']/ancestor::li")).click();
-        driver.findElement(By.xpath("//a[@title='Faded Short Sleeve T-shirts']/ancestor::li")).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("Submit"))).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='layer_cart']//a[@class and @title='Proceed to checkout']"))).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[contains(@class,'cart_navigation')]/a[@title='Proceed to checkout']"))).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("processAddress"))).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("uniform-cgv"))).click();
-        driver.findElement(By.name("processCarrier")).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("bankwire"))).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='cart_navigation']/button"))).click();
-        WebElement heading = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("h1")));
+        $(".login").click();
+        $(By.id("email")).sendKeys(existingUserEmail);
+        $(By.id("passwd")).sendKeys(existingUserPassword);
+        $(By.id("SubmitLogin")).click();
+        $(By.linkText("Women")).click();
+        $(By.xpath("//a[@title='Faded Short Sleeve T-shirts']/ancestor::li")).click();
+        $(By.xpath("//a[@title='Faded Short Sleeve T-shirts']/ancestor::li")).click();
+        $(By.name("Submit")).click();
+        $(By.xpath("//*[@id='layer_cart']//a[@class and @title='Proceed to checkout']")).click();
+        $(By.xpath("//*[contains(@class,'cart_navigation')]/a[@title='Proceed to checkout']")).click();
+        $(By.name("processAddress")).click();
+        $(By.id("uniform-cgv")).click();
+        $(By.name("processCarrier")).click();
+        $(By.className("bankwire")).click();
+        $(By.xpath("//*[@id='cart_navigation']/button")).click();
+        $("h1").waitUntil(Condition.visible, 1000).shouldHave(Condition.text("ORDER CONFIRMATION"));
+        $(By.xpath("//li[@class='step_done step_done_last four']")).shouldBe(Condition.visible);
+        $(By.xpath("//li[@id='step_end' and @class='step_current last']")).shouldBe(Condition.visible);
+        $(By.xpath("//*[@class='cheque-indent']/strong")).text().contains("Your order on My Store is complete.");
+        assertTrue(url().contains("controller=order-confirmation"));
+    }
 
-        assertEquals("ORDER CONFIRMATION", heading.getText());
-        assertTrue(driver.findElement(By.xpath("//li[@class='step_done step_done_last four']")).isDisplayed());
-        assertTrue(driver.findElement(By.xpath("//li[@id='step_end' and @class='step_current last']")).isDisplayed());
-        assertTrue(driver.findElement(By.xpath("//*[@class='cheque-indent']/strong")).getText().contains("Your order on My Store is complete."));
-        assertTrue(driver.getCurrentUrl().contains("controller=order-confirmation"));
-    }*/
 }
